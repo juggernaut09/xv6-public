46f2fd220332f9e949a8f3c78638445ec765a193
juggernaut09 <venkatteja.ravi@gmail.com>
2023-09-10 00:32:00 -0400

    Added comments in dataRead: uniq.c

--- uniq.c
+++ uniq.c
@@ -4,181 +4,188 @@
 #include "user.h"
 
 
-// char *arr[1024];
-// int length = 0;
+char *arr[1024];
+int length = 0;
 
-// // Memory Allocation
-// void init(){
-//     for(int i = 0; i < 1024; i++){
-//         arr[i] = malloc(100);
-//         }
-// }
+// Memory Allocation
+void init(){
+    for(int i = 0; i < 1024; i++){
+        arr[i] = malloc(100);
+        }
+}
 
-// void dataRead(char *fname){
-//     int file = 0, inp;
-//     int i, index=0, counter=0;
-//     char buffer[1024]; 
-//     char buffer1[1024];
-//     file = open(fname, O_RDONLY);
-//     while((inp = read(file, buffer, 1024)) != 0){
-//         for (i = 0; i < inp; i++)
-//         {
-//             if(buffer[i] != '\n'){
-//                 buffer1[index++] = buffer[i];      
-//             }else{
-//                 strcpy(arr[counter++], buffer1);
-//                 memset(buffer1, '\0', 1024);
-//                 length++;
-//                 index = 0;
-//             }
-//         }
-//     }
-// }
+void dataRead(char *fname){
+    // This function intitializes arr which is an array of strings 
+    //["I understand the Operating system.", "I understand the Operating system.", "I understand the Operating system.", 
+    //"I love to work on OS.", "I love to work on OS.", "Thanks xv6"].
+    int file = 0, number_of_bytes_read;
+    int i, index=0, counter=0;
+    char buffer[1024]; 
+    char buffer1[1024];
+    file = open(fname, O_RDONLY);
+    // no of bytes to read = 1024
+    // buffer will have 1024 bytes read in a single stride.
+    while((number_of_bytes_read = read(file, buffer, 1024)) != 0){
+        for (i = 0; i < number_of_bytes_read; i++) // Looping through the characters read in single stride i.e. 1024 characters
+        {
+            if(buffer[i] != '\n'){ // Check if the reading character is new line sequence or not
+                buffer1[index] = buffer[i]; // if it is a valid character then store in the buffer1 char array.
+                index = index + 1; // increment the index.     
+            }else{ // If it reaches the new line i.e a complete string.
+                strcpy(arr[counter], buffer1); //Store that string in arr.
+                counter = counter + 1;
+                memset(buffer1, '\0', 1024); // Make the buffer1 empty. remove the string from buffer1 which was just now moved to arr.  
+                length++;
+                index = 0;
+            }
+        }
+    }
+}
 
-// // Basic uniq functionality
-// void uniqbasic(){
-//     char buffer[100];
-//     for(int i = 0; i < length; i++){
-//         strcpy(buffer, arr[i]);  // Copying the strings to the buffer for the comparison
-//         if(strcmp(buffer, arr[i+1]) == 0){
-//             continue;  // If the consective lines are same, continue with the next strings
-//         }else{
-//             printf(1, "%s\n", buffer);
-//         }
-//     }
-// }
-
-
-// // keeping a count of the entered strings
-// void uniqcfunction(){
-//     char buffer[100];
-//     int count = 1; 
-//     for(int i = 0; i < length; i++){
-//         strcpy(buffer, arr[i]);
-//         if(strcmp(buffer, arr[i+1]) == 0){
-//             count++; // If both the lines are same, we increment the count  
-//             continue;
-//         }else{
-//             printf(1, "%d %s\n",count, buffer); 
-//             count = 1;
-//         }
-//     }
-// }
+// Basic uniq functionality
+void uniqbasic(){
+    char buffer[100];
+    for(int i = 0; i < length; i++){
+        strcpy(buffer, arr[i]);  // Copying the strings to the buffer for the comparison
+        if(strcmp(buffer, arr[i+1]) == 0){
+            continue;  // If the consective lines are same, continue with the next strings
+        }else{
+            printf(1, "%s\n", buffer);
+        }
+    }
+}
 
 
-// // printing only the duplicate lines
-// void uniqdfunction(){
-//     char buffer[100];
-//     int flag = 0;
-//     for(int i = 0; i < length; i++){
-//         strcpy(buffer, arr[i]);
-//         if(strcmp(buffer, arr[i+1]) == 0){
-//             flag = 1; // using flag to note that two lines are same
-//             continue;
-//         }else{
-//             if(flag == 1)
-//             printf(1, "%s\n", buffer);
-//             flag = 0;
-//         }
-//     }
-// }
+// keeping a count of the entered strings
+void uniqcfunction(){
+    char buffer[100];
+    int count = 1; 
+    for(int i = 0; i < length; i++){
+        strcpy(buffer, arr[i]);
+        if(strcmp(buffer, arr[i+1]) == 0){
+            count++; // If both the lines are same, we increment the count  
+            continue;
+        }else{
+            printf(1, "%d %s\n",count, buffer); 
+            count = 1;
+        }
+    }
+}
 
 
-// // ignoring the case sensitivity 
-// void uniqifunction(){
-//     char buffer[100];
-//     char temp[100];
-//     char next[100];
-//     int i=0;
-//     for(i = 0; i < length; i++){
-//         strcpy(buffer, arr[i]);
-//         strcpy(temp, buffer);
-//         strcpy(next, arr[i + 1]);
+// printing only the duplicate lines
+void uniqdfunction(){
+    char buffer[100];
+    int flag = 0;
+    for(int i = 0; i < length; i++){
+        strcpy(buffer, arr[i]);
+        if(strcmp(buffer, arr[i+1]) == 0){
+            flag = 1; // using flag to note that two lines are same
+            continue;
+        }else{
+            if(flag == 1)
+            printf(1, "%s\n", buffer);
+            flag = 0;
+        }
+    }
+}
 
-//         // converting all the characters in the string to the lower case 
-//         for(int j=0;buffer[j]!='\0';j++){
-//             if(buffer[j]>=65 && buffer[j]<=90){
-//                 buffer[j]=buffer[j]+32;
-//             }
-//         }
 
-//         for(int j=0;next[j]!='\0';j++){
-//             if(next[j]>=65 && next[j]<=90){
-//                 next[j]=next[j]+32;
-//             }
-//         }
+// ignoring the case sensitivity 
+void uniqifunction(){
+    char buffer[100];
+    char temp[100];
+    char next[100];
+    int i=0;
+    for(i = 0; i < length; i++){
+        strcpy(buffer, arr[i]);
+        strcpy(temp, buffer);
+        strcpy(next, arr[i + 1]);
 
-//         // comparing both the cases and printing only the unique string
-//         if(strcmp(buffer, next) == 0){
-//             continue;
-//         }else{
-//             printf(1, "%s\n", temp);
-//         }
-//     }
-// }
+        // converting all the characters in the string to the lower case 
+        for(int j=0;buffer[j]!='\0';j++){
+            if(buffer[j]>=65 && buffer[j]<=90){
+                buffer[j]=buffer[j]+32;
+            }
+        }
 
-// // redirecting the output of the cat command to input of the uniq 
-// void pipeFunction(){
-//     init();
-//     char buffer[1024],buffer1[1024];
-//         int inp,i, index=0;
-//         int counter=0;
-//         while((inp = read(0, buffer, sizeof(buffer))) > 0) {
-//             for (i = 0; i < inp; i++)
-//             {
-//                 if(buffer[i] != '\n'){
+        for(int j=0;next[j]!='\0';j++){
+            if(next[j]>=65 && next[j]<=90){
+                next[j]=next[j]+32;
+            }
+        }
+
+        // comparing both the cases and printing only the unique string
+        if(strcmp(buffer, next) == 0){
+            continue;
+        }else{
+            printf(1, "%s\n", temp);
+        }
+    }
+}
+
+// redirecting the output of the cat command to input of the uniq 
+void pipeFunction(){
+    init();
+    char buffer[1024],buffer1[1024];
+        int inp,i, index=0;
+        int counter=0;
+        while((inp = read(0, buffer, sizeof(buffer))) > 0) {
+            for (i = 0; i < inp; i++)
+            {
+                if(buffer[i] != '\n'){
                     
-//                     buffer1[index++] = buffer[i];
+                    buffer1[index++] = buffer[i];
                         
-//                 }else{
-//                     strcpy(arr[counter++], buffer1);
-//                     memset(buffer1, '\0', 1024);
-//                     length++;
-//                     index = 0;
-//                 }
-//             }
-//         }
-//         uniqbasic();
-// }
+                }else{
+                    strcpy(arr[counter++], buffer1);
+                    memset(buffer1, '\0', 1024);
+                    length++;
+                    index = 0;
+                }
+            }
+        }
+        uniqbasic();
+}
 
 
-// // main function
-// int main(int argc, char* argv[]){
-//     if(argc<=1){
-//         pipeFunction();
-//         }
-//     if(argc == 2){
-//         init();
-//         dataRead(argv[1]);
-//         uniqbasic();
-//     }
-//     if(argc == 3){
-//         if(strcmp(argv[1], "-d") == 0){
-//              init();
-//         dataRead(argv[2]);
-//         uniqdfunction();
-//         }
-//         if(strcmp(argv[1], "-i") == 0){
-//              init();
-//             dataRead(argv[2]);
-//             uniqifunction();
-//         }
-//         if(strcmp(argv[1], "-c") == 0){
-//              init();
-//         dataRead(argv[2]);
-//         uniqcfunction();
-//         }
-//     if(argc > 3){
-//         printf(1, "Too many arguments given");
-//     }
-//     }
-//    exit();
-// }
+// main function
+int main(int argc, char* argv[]){
+    if(argc<=1){
+        pipeFunction();
+        }
+    if(argc == 2){
+        init();
+        dataRead(argv[1]);
+        uniqbasic();
+    }
+    if(argc == 3){
+        if(strcmp(argv[1], "-d") == 0){
+             init();
+        dataRead(argv[2]);
+        uniqdfunction();
+        }
+        if(strcmp(argv[1], "-i") == 0){
+             init();
+            dataRead(argv[2]);
+            uniqifunction();
+        }
+        if(strcmp(argv[1], "-c") == 0){
+             init();
+        dataRead(argv[2]);
+        uniqcfunction();
+        }
+    if(argc > 3){
+        printf(1, "Too many arguments given");
+    }
+    }
+   exit();
+}
 
-
-int 
-main(int argc, char* argv[]) {
-    printf(1, "return val of system call is %d\n", uniq(argc, argv));
-    printf(1, "Congrats !! You have successfully added new system  call in xv6 OS :) \n");
-    exit();
- }
+// int 
+// main(int argc, char* argv[]) {
+//     // printf(1, "return val of system call is %d\n", uniq(argc, argv));
+//     // printf(1, "Congrats !! You have successfully added new system  call in xv6 OS :) \n");
+//     uniq(argc, argv);
+//     exit();
+//  }
